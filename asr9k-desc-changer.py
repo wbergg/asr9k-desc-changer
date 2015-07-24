6d45e597279553d90268cab1a9b2755459ae200c
wbergg <alex.wagberg@gmail.com>
2015-07-24 14:02:10 +0200

    total remake, why make a 2-step rocket when you can do all changes directly after parsing

--- asr9k-desc-changer.py
+++ asr9k-desc-changer.py
@@ -3,21 +3,16 @@
 import paramiko
 import time
 
-def device_list():
-    file_input = open('routers.txt', 'r')
+def main():
+    file_input = open('routerrs.txt', 'r')
     device_list = file_input.read()
-    device_list.splitlines()
-    for line in device_list.splitlines():
-        device_list = device_list.strip()
-    return device_list
-
-def get_data(device_list):
-    device_list = device_list.split("\n")
-    output = []
+    device_list = device_list.strip().split("\n")
     username = 'xxx'
     password = 'xxx'
     for device in device_list:
-        print "At device: " % device
+        output = list()
+        output2 = list()
+        print "Getting configuration from %s" % device
         remote_conn_pre=paramiko.SSHClient()
         remote_conn_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         remote_conn_pre.connect(device, username=username, password=password)
@@ -27,26 +22,35 @@ def get_data(device_list):
         time.sleep(1)
         router_data = remote_conn.recv(5000)
         output.append(router_data)
-    return output
-
-def parse(router_data):
-    output = []
-    for line in router_data:
-        data = line.strip().split("\n")
-        for words in data:
-            if '"' in words:
-                words = (words.replace('"', ''))
-                output.append(words.split(None,3))
-
-    for line in output:
-        if 'BE' in line[0]:
-            line[0] = (line[0].replace('BE', 'Bundle-Ethernet'))
-    return output
-
-def main():
-    device = device_list()
-    router_data = get_data(device)
-    mongo = parse(router_data)
+        for line in output:
+            data = line.strip().split("\n")
+            for words in data:
+                if '"' in words:
+                    words = (words.replace('"', ''))
+                    output2.append(words.split(None,3))
+        for line in output2:
+            if 'BE' in line[0]:
+                line[0] = (line[0].replace('BE', 'Bundle-Ether'))
+        if len(output2) == 0:
+            print "No config change required for %s" % device
+        else:
+            print "Will now send configuration to %s" % device
+            remote_conn.send("conf t\n")
+            for line in output2:
+                remote_conn.send("interface %s\n" % line[0])
+                remote_conn.send("description %s\n" % line[3])
+                time.sleep(1)
+                router_output = remote_conn.recv(5000)
+                #For debug purpose, if you want to see what's actually sent
+                #print router_output
+            remote_conn.send("commit\n")
+            router_output = remote_conn.recv(1000)
+            if len(router_output) == 39:
+                print "Done with %s, everything went OK!" % device
+            else:
+                print "Something went wrong with the commit in %s" % device
+            remote_conn.close()
 
 if __name__ == "__main__":
     main()
+
